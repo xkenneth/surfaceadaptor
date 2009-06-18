e0ab77c8a1424fcfec6a977408b907237026aefc
TeleDrill <teledrill@tdsurface-a-secondary.(none)>
2009-06-18 16:53:40 -0500

    Fixed up helper.py

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import math
+
 ignore = ['us_flow_ready','depth_1','depth_2']
 
 #axis = ['x','y','z']
@@ -13,7 +15,11 @@ def convert(name, value):
             value = (float(value)/100.0)*5.0
 
     elif name == 'toolface':
-        value = (float(value)/10000.0)*360.0
+        if res == 'highres':
+            value = (float(value)/10000.0)*360.0
+        elif res == 'lowres':
+            value = (float(value)/100.0)*360.0
+            
 
     elif name == 'toolface':
         value = (float(value)/10000.0)*360.0
